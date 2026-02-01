f609adb9e1c08bdeb069607080f128fe9a3f46e9
Bagonty <dm.rekarg@gmail.com>
2026-02-01 14:12:36 +0500

    add booleans

---
+++ 1.py
@@ -0,0 +1,67 @@
+#Boolean Values
+#1
+print(100 > 99)
+print(1 == 5)
+print(45 < 334)
+print(2<=7)
+print(5!=5)
+
+#2
+a= 10
+b=20
+if b>a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+
+d=15
+e=40
+if d <=e:
+    print("d is less than or equal to e")
+else:
+    print("d is greater than e")
+
+
+f=35
+g= 7
+if f==g:
+    print("f is equal to g")
+else:
+    print("f is not equal to g")
+
+h=99
+i=3
+if h%i==0:
+    print("h is divisible by i")
+else:
+    print("h is not divisible by i")
+
+j = 64
+k = 8
+if j**0.5 == k :
+    print("k is the square root of j")
+else:
+    print("k is not the square root of j")
+
+#3
+
+print(bool(" Bagaaaa"))
+print(bool(900))
+print(bool(["football", "basketball", "voleyball"]))
+print(bool(3.14))
+print(bool({"name": "Baga", "age": 18}))
+
+
+#4
+x="Baga_krasava"
+y=59
+z=["python", "java", "c++"]
+t={"city": "Almaty", "country": "Kazakhstan"}
+s=3.14159265358979323846
+
+
+print(bool(x))
+print(bool(y))
+print(bool(z))
+print(bool(t))
+print(bool(s))
